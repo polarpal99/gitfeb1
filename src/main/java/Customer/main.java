eed8025c1cf2602adba8777ccc332bc9a55abb68
polarpal99 <52849158+polarpal99@users.noreply.github.com>
2020-02-05 20:16:10 -0600

    reload flatter filetree

---
+++ src/main/java/Customer/main.java
@@ -0,0 +1,18 @@
+package Customer;
+
+
+/**
+ *
+ * @author polar
+ */
+class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello, World - Feb1a Project HERE!"); 
+        //customer = New Customer;
+        
+    } 
+} 
